639f91696c5d5df073c3418d67f9c470cad87091
pnzrr <pnzrrr@pm.me>
2023-05-08 22:58:28 -0600

    realm

--- apps/admin-ui/src/realm/routes/AddRealm.tsx
+++ apps/admin-ui/src/realm/routes/AddRealm.tsx
@@ -1,15 +1,19 @@
 import { lazy } from "react";
 import type { Path } from "react-router-dom";
 import { generatePath } from "react-router-dom";
-import type { RouteDef } from "../../route-config";
+import type { AppRouteObject } from "../../routes";
 
 export type AddRealmParams = { realm: string };
 
-export const AddRealmRoute: RouteDef = {
+const NewRealmForm = lazy(() => import("../add/NewRealmForm"));
+
+export const AddRealmRoute: AppRouteObject = {
   path: "/:realm/add-realm",
-  component: lazy(() => import("../add/NewRealmForm")),
+  element: <NewRealmForm />,
   breadcrumb: (t) => t("realm:createRealm"),
-  access: "view-realm",
+  handle: {
+    access: "view-realm",
+  },
 };
 
 export const toAddRealm = (params: AddRealmParams): Partial<Path> => ({
